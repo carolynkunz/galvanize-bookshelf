b31602306f830f7b7375ba5cf6840e0f97adcecd
carolynkunz <carolyn.delbeccaro@gmail.com>
2016-10-07 11:46:21 -0700

    Prepare the Heroku

---
+++ techEventsAPI_notes.js
@@ -0,0 +1,19 @@
+https://api.meetup.com/2/open_events? + zip + &and_text=False&offset=0 + city + &format=json&limited_events=True + topic + &photo-host=public&page=20 + radius + &desc=False&status=upcoming + API key
+
+zip=78701
+
+&and_text=False&offset=0
+
+&city=austin
+
+&format=json&limited_events=True
+
+&text=javascript
+
+&photo-host=public&page=20
+
+&radius=5
+
+&desc=False&status=upcoming
+
+&sig_id=211406069&sig=e8b61bdc0c6ab12bacb58a58d29a66b61b1aafb2
